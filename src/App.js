cd3dacd3192032b593d180010130eaf9860d1cb4
Tyler Williams <tyler.williams@spothero.com>
2021-02-28 16:21:23 -0600

    shutup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { Button } from "@spothero/ui";
+import { Button as ButtonV1, ThemeProvider } from "@spothero/chakra";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          width: "100vw",
+          height: "100vh",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <h2 style={{ textAlign: "center" }}>Spothero UI</h2>
+          <Button color="primary">Hello</Button>
+        </div>
+        <div>
+          <h2 style={{ textAlign: "center" }}>Spothero UI V2</h2>
+          <ButtonV1 color="primary">Hello</ButtonV1>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "scrollingelement";
+import "transitionEnd";
+
+render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
