0807e773428c3ac67efbaf04a79aaaa7f0511f77
Tyler Williams <tyler.williams@spothero.com>
2021-03-02 10:37:26 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,32 +1,71 @@
 import React from "react";
 import "./App.scss";
 
-import { ChakraProvider } from "@chakra-ui/react";
-import { Button } from "@spothero/ui";
-import { Button as ButtonV1, ThemeProvider } from "@spothero/chakra";
+import {
+  Button as ChakraButton,
+  Heading as ChakraHeading,
+} from "@chakra-ui/react";
+
+import Heading from "fe-ui/v2/Heading";
+import ButtonV2 from "fe-ui/v2/Button";
+import ThemeProvider from "fe-ui/v2/ThemeProvider";
+import ThemeProvider2 from "@spothero/chakra/v2/ThemeProvider";
+import Button from "@spothero/chakra/v1/Button";
+
+const GridBox = (props) => (
+  <div
+    style={{
+      width: "100%",
+      height: "100%",
+      padding: "1rem",
+      border: "1px solid #ccc",
+    }}
+    {...props}
+  />
+);
 
 function App() {
+  // console.log(SHTheme);
   return (
-    <ThemeProvider>
+    <div>
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "1fr 1fr",
+          gridTemplateRows: "repeat(2, 50vh)",
+          gridTemplateColumns: "repeat(2, 50vw)",
           width: "100vw",
           height: "100vh",
           justifyContent: "center",
         }}
       >
-        <div>
-          <h2 style={{ textAlign: "center" }}>Spothero UI</h2>
-          <Button color="primary">Hello</Button>
-        </div>
-        <div>
-          <h2 style={{ textAlign: "center" }}>Spothero UI V2</h2>
-          <ButtonV1 color="primary">Hello</ButtonV1>
-        </div>
+        <GridBox>
+          <ChakraHeading textAlign="center" as="h1">
+            Chakra Default
+          </ChakraHeading>
+          <ChakraButton>Some Button</ChakraButton>
+        </GridBox>
+        <GridBox>
+          <h1>Normal H1 Tag</h1>
+          <Button color="primary">FE UI V1</Button>
+        </GridBox>
+        <ThemeProvider>
+          <GridBox>
+            <Heading textAlign="center" as="h1" styledAs="h1">
+              SpotheroTheme Bare Repo
+            </Heading>
+            <ButtonV2 variant="primary">Hello</ButtonV2>
+          </GridBox>
+        </ThemeProvider>
+        <ThemeProvider2>
+          <GridBox>
+            <Heading textAlign="center" as="h1" styledAs="h1">
+              SpotheroTheme FE-UI V2
+            </Heading>
+            <ButtonV2 variant="primary">Hello</ButtonV2>
+          </GridBox>
+        </ThemeProvider2>
       </div>
-    </ThemeProvider>
+    </div>
   );
 }
 
